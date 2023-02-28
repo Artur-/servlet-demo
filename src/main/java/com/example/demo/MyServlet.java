f9961bcf066ac7a903f81de83364479402a5b561
Artur Signell <artur@vaadin.com>
2023-02-28 19:11:42 +0200

    Test

---
+++ src/main/java/com/example/demo/MyServlet.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/*")
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writeLine(writer, "<pre>");
+		writeLine(writer, "Request URI: " + req.getRequestURI());
+		writeLine(writer, "Request URL: " + req.getRequestURL());
+		writeLine(writer, "Request path info: " + req.getPathInfo());
+		writeLine(writer, "Request path translated: " + req.getPathTranslated());
+		writeLine(writer, "Request context path: " + req.getContextPath());
+		writeLine(writer, "</pre>");
+		resp.setContentType("text/html");
+	}
+
+	private void writeLine(PrintWriter writer, String string) {
+		writer.write(string + "\n");
+	}
+}
